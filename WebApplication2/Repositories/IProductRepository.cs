5944a46cf6936de86acac658c5381deed564667b
long trần <pelong16042004@gmail.com>
2024-12-09 23:45:42 +0700

    Add project files.

---
+++ WebApplication2/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Repositories
+{
+    
+    
+        public interface IProductRepository
+        {
+            Task<IEnumerable<Product>> GetAllAsync();
+            Task<Product> GetByIdAsync(int id);
+            Task AddAsync(Product product);
+            Task UpdateAsync(Product product);
+            Task DeleteAsync(int id);
+        }
+    
+}
